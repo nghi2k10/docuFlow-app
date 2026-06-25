52cee25539cc7b4643bb9e1363d2be1ad5cf72cd
nghi2k10 <nhnghi2001@gmail.com>
2026-06-25 20:57:52 +0700

    del auth

--- src/App.jsx
+++ src/App.jsx
@@ -3,29 +3,24 @@ import { Toaster as HotToaster } from 'react-hot-toast'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import PageNotFound from './lib/PageNotFound';
-import { AuthProvider } from '@/lib/AuthContext';
 import ScrollToTop from './components/ScrollToTop';
-// Add page imports here
 import Home from './pages/Home';
 import Viewer from './pages/Viewer';
 
 function App() {
   return (
-    <AuthProvider>
-      <QueryClientProvider client={queryClientInstance}>
-        <Router>
-          <ScrollToTop />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/viewer" element={<Viewer />} />
-            <Route path="*" element={<PageNotFound />} />
-          </Routes>
-        </Router>
-        <Toaster />
-        <HotToaster position="bottom-center" />
-      </QueryClientProvider>
-    </AuthProvider>
+    <QueryClientProvider client={queryClientInstance}>
+      <Router>
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/viewer" element={<Viewer />} />
+          <Route path="*" element={<Home />} />
+        </Routes>
+      </Router>
+      <Toaster />
+      <HotToaster position="bottom-center" />
+    </QueryClientProvider>
   )
 }
 
--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"), // ← thêm dòng này
+      '@': path.resolve(__dirname, './src'),
     },
   },
 })
